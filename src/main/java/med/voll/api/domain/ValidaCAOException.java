311c17b6c28cd1ddbd193fbad3d8464c340d3525
diogo.brito <britu.diogo@gmaiil.com>
2023-12-06 16:25:40 -0300

    Inicio_SpringBoot-03

---
+++ src/main/java/med/voll/api/domain/ValidaCAOException.java
@@ -0,0 +1,7 @@
+package med.voll.api.domain;
+
+public class ValidaCAOException extends RuntimeException {
+    public ValidaCAOException(String mensagem) {
+        super(mensagem);
+    }
+}
--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsulta.java
@@ -1,13 +1,32 @@
 package med.voll.api.domain.consulta;
 
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.ValidaCAOException;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendaDeConsulta {
     @Autowired
+    private ConsultaRepository consultaRepository;
+    @Autowired
+    private MedicoRepository medicoRepository;
+    @Autowired
+    private PacienteRepository pacienteRepository;
+    @Autowired
     public void agendar (DadosAgendamentoConsulta dados){
+        if(!pacienteRepository.existsById(dados.idPaciente())){
+            throw  new ValidaCAOException("ID DO PACIENTE INFORMADO NÃO EXISTE");
+        }
+        if(!medicoRepository.existsById(dados.idMedico())){
+            throw  new ValidaCAOException("ID DO MEDICO INFORMADO NÃO EXISTE");
+        }
 
-
+        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var consulta = new Consulta(null, medico, paciente, dados.data());
+        consultaRepository.save(consulta);
     }
 }
