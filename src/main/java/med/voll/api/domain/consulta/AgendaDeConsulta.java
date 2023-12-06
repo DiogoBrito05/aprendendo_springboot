66154ca8e106eb13327fb5dfc0be9a7c552f5d03
diogo.brito <britu.diogo@gmaiil.com>
2023-12-06 17:01:55 -0300

    Inicio_SpringBoot-03

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsulta.java
@@ -2,6 +2,7 @@ package med.voll.api.domain.consulta;
 
 import jakarta.validation.ValidationException;
 import med.voll.api.domain.ValidaCAOException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +21,32 @@ public class AgendaDeConsulta {
         if(!pacienteRepository.existsById(dados.idPaciente())){
             throw  new ValidaCAOException("ID DO PACIENTE INFORMADO NÃO EXISTE");
         }
-        if(!medicoRepository.existsById(dados.idMedico())){
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
             throw  new ValidaCAOException("ID DO MEDICO INFORMADO NÃO EXISTE");
         }
-
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
-        var medico = medicoRepository.findById(dados.idMedico()).get();
-        var consulta = new Consulta(null, medico, paciente, dados.data());
+        var medico = escolherMedico(dados);
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
     }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados){
+        if(dados.idMedico() != null){
+            return  medicoRepository.getReferenceById(dados.idMedico());
+        }
+        if(dados.especialidade() == null){
+            throw  new ValidaCAOException("Especialidade é obrigatória quando médico não for escolhido");
+        }
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidaCAOException("Id da consulta informado não existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
+    }
+
 }
