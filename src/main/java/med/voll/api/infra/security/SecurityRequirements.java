a3069deb76a8bf840f7de859d9c51743649d7bdd
diogo.brito <britu.diogo@gmaiil.com>
2023-12-08 11:55:25 -0300

    Inicio_SpringBoot-03

--- src/main/java/med/voll/api/infra/security/SecurityRequirements.java
+++
@@ -1,7 +0,0 @@
-//package med.voll.api.infra.security;
-//
-//import java.lang.annotation.Annotation;
-//
-//public interface SecurityRequirements extends Annotation {
-//    SecurityRequirement[] value() default {};
-//}
