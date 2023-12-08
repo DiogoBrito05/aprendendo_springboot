2bd5142a150efefcdd9e4b2149eb5874bfccf690
diogo.brito <britu.diogo@gmaiil.com>
2023-12-08 16:59:48 -0300

    Inicio_SpringBoot-03

--- src/main/java/med/voll/api/infra/security/springdoc/springDocConfigurations.java
+++ src/main/java/med/voll/api/infra/security/springdoc/springDocConfigurations.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class springDocConfigurations {
 
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
